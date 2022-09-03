6254b0d98887d3fc3678f983dac4e5446f4780ee
carmeloyip <yesonghao@email.com>
2022-09-03 15:21:02 +0800

    version 2.0 修改了WebServelt

--- src/main/java/TestServlet.java
+++ src/main/java/TestServlet.java
@@ -5,7 +5,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet("/TestServlet")
+@WebServlet("/testServlet")
 public class TestServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.getWriter().println("hello world");
